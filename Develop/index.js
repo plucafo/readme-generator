7a7b35a7e306e61a1ab1e23afe83cbe5a86c8891
plucafo <plucaf87@gmail.com>
2024-02-19 08:47:06 -0700

    created package.json, installed inquirer package and created working test prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,37 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your username:',
+      },
+      {
+        type: 'list',
+        name: 'color',
+        message: 'Pick a color:',
+        choices: ['Red', 'Green', 'Blue'],
+      },
+      {
+        type: 'confirm',
+        name: 'confirm',
+        message: 'Are you sure?',
+        default: true,
+      },
+  ])
+  .then((answers) => {
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 // TODO: Create an array of questions for user input
 const questions = [];
 
